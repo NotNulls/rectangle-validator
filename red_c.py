2e399ebc484007d7071e023997629cbabdb38a90
Dusan Rangelov <dusanrangelov@yahoo.com>
2024-04-24 18:17:46 +0200

    Second commit: If rectangle

--- red_c.py
+++ red_c.py
@@ -8,78 +8,73 @@ def cordinates_from_file(filename):
         coordinates = [list(map(float, line.strip().split(','))) for line in lines]
     return coordinates
 
-def check_rectagle(A,B,C):
-    """
-    Firstly we shall calculate the vectors formded by those points given and find out about point D coordinates.
-    """
-
-    AB = np.array(B) - np.array(A)
-    BC = np.array(C) - np.array(B)
-    
-    
-
-    if AB @ BC == 0:
-        print ('Točke obrazuju pravokut.')
-    else:
-        print ('Točke ne obrazuju pravokut. ')
-        
-
-
 def fourthPointCoordiantes(A,B,C):
-
     """
     Pronalazi koordinate nedostajuće točke.
     """
+
+    AC = np.array(C) - np.array(A)
+    D = (tuple(np.array(B) + AC))
     
-
-    mid_point = ((A[0] + C[0]) / 2, (A[1] + C[1]) / 2)
-    print ('Mid Point: ',mid_point)
-
-    D = (float(2 * mid_point[0] - B[0]), float(2 * mid_point[1] - B[1]))
-
     print (f'Točka D je kordinata: {D}')
     return (D)
 
-    
-
-A = (1,1)
-B = (1,3)
-C = (5,5)
-
-
 
 def findDiagonalLenght(A,C):
-    diagonal = math.sqrt((C[0]-A[0])**2 + (C[1]-A[1])**2)
-    print (f'Dujina diagonale je: {diagonal}')
 
+    A = np.array(A)
+    C = np.array(C)
 
+    diagonal_ad = math.sqrt((D[0]-A[0])**2 + (D[1]-A[1])**2)
+    diagonal_cb = math.sqrt((C[0]-B[0])**2 + (C[1]-B[1])**2)
+    
+    print(f'Diagonala AD: {diagonal_ad}, diagonala CB: {diagonal_cb}')
+    return diagonal_ad,diagonal_cb
+    
+def midPoint(A,B,C,D):
+    mid_point_BC = ((B[0] + C[0]) / 2, (B[1] + C[1]) / 2)
+    mid_point_AD = ((A[0] + D[0]) / 2, (A[1] + D[1]) / 2)
 
+    return mid_point_AD, mid_point_BC
 
 
 def isPointInRectangle(A,C,X=None):
-    if (X[0]>A[0] and X[0]<C[0] and X[1]<C[1] and X[1]<C[1]):
+    if (X[0]>A[0] and X[0]<D[0] and X[1]<C[1] and X[1]<D[1]):
         print ('Točka je unutar datog pravokuta.')
     else:
         print ('Točka je van datog pravokuta.')
 
+def is_rectangle(mid_point_AD, mid_point_BC):
+        if mid_point_AD == mid_point_BC and diagonal_ad == diagonal_cb:
+            print ('Postavljene tačke obrazuju pravokut.')
+            return True
+        else:
+            print ('Postavljene tačke ne obrazuju pravokut.')
+            return False
+             
 
 
 if __name__ == '__main__':
 
-    # if check_rectagle(A,B,C) == True:
-    #     fourthPointCoordiantes(A,B,C)
-    #     isPointInRectangle(A,C,X=(0,4))
-    #     findDiagonalLenght(A,C)
-
-
-   
 
     
+    A = (1,1)
+    B = (6,1)
+    C = (1,6)
+    X = (2,2)
 
-    if check_rectagle(A,B,C) == True:
-        isPointInRectangle(A,C,X=(0,4))
-    elif fourthPointCoordiantes(A,B,C):
-        
-        
+    D = fourthPointCoordiantes(A,B,C)
+    
+    diagonal_ad,diagonal_cb = findDiagonalLenght(A,C)
+    
+    mid_BC, mid_AD = midPoint(A,B,C,D)
 
-        d = findDiagonalLenght(A,C)
+    is_rectangle(mid_BC, mid_AD)
+        
+    
+    in_rectangle = isPointInRectangle(A,D,X)
+    
+    
+
+    
+        
