5a4bafa95eb9e09a2864ab6783d3bd6e9fe5b0b8
Dusan Rangelov <dusanrangelov@yahoo.com>
2024-04-24 22:47:46 +0200

    Fourth commit: CSV loop

--- red_c.py
+++ red_c.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def fourth_point_coordinates(a, b, c):
     """
-    Find coordinates of the fourth point for a given three points of a rectangle.
+    Pronalaženje četvrte točke.
     """
     ac = np.array(c) - np.array(a)
     d = tuple(np.array(b) + ac)
@@ -13,7 +13,7 @@ def fourth_point_coordinates(a, b, c):
 
 def find_diagonal_length(a, c, d):
     """
-    Find the lengths of diagonals.
+    Pronalaženje dujina diagonala.
     """
     diagonal_ad = math.sqrt((d[0] - a[0]) ** 2 + (d[1] - a[1]) ** 2)
     diagonal_cb = math.sqrt((c[0] - b[0]) ** 2 + (c[1] - b[1]) ** 2)
@@ -22,7 +22,7 @@ def find_diagonal_length(a, c, d):
 
 def mid_point(a, b, c, d):
     """
-    Find the midpoints of diagonals.
+    Pronalaženje središnjih točaka.
     """
     midpoint_bc = ((b[0] + c[0]) / 2, (b[1] + c[1]) / 2)
     midpoint_ad = ((a[0] + d[0]) / 2, (a[1] + d[1]) / 2)
@@ -31,7 +31,7 @@ def mid_point(a, b, c, d):
 
 def is_point_in_rectangle(a, d, x):
     """
-    Check if a point is within a rectangle defined by given points.
+    Provera da li je točka unutar pravokuta.
     """
     if (x[0] > a[0] and x[0] < d[0] and x[1] > a[1] and x[1] < d[1]):
         print('Točka je unutar pravokuta.')
@@ -41,34 +41,47 @@ def is_point_in_rectangle(a, d, x):
 
 def is_rectangle(midpoint_ad, midpoint_bc, diagonal_ad, diagonal_cb):
     """
-    Check if points form a rectangle based on midpoints and diagonal lengths.
+    Proveravanje da li točke formiraju pravokut na osnovu središnjih točaka i diagonala.
     """
     if midpoint_ad == midpoint_bc and diagonal_ad == diagonal_cb:
         return True
     else:
         return False
 
+def read_coordinates_from_file(filename):
+    coordinates = []
+    with open(filename, 'r') as file:
+        for line in file:
+            # Pretvoriti svaku liniju ponaosob u tuple koji sadrži float članove
+            cleaned_line = line.strip().replace("(", "").replace(")", "")
+            coord_tuple = tuple(map(float, cleaned_line.split(',')))
+            coordinates.append(coord_tuple)
+    return coordinates
 
 if __name__ == '__main__':
-    a = (1, 1)
-    b = (6, 1)
-    c = (1, 6)
-    x = (2, 2)
+    
+    coordinates = read_coordinates_from_file('dummydata.csv')
+    for coord in coordinates:
+        a = tuple(coord[0:2])  
+        b = tuple(coord[2:4])  
+        c = tuple(coord[4:6])  
+        x = tuple(coord[6:8])
 
-    d = fourth_point_coordinates(a, b, c)
+        d = fourth_point_coordinates(a, b, c)
 
-    diagonal_ad, diagonal_cb = find_diagonal_length(a, c, d)
+        diagonal_ad, diagonal_cb = find_diagonal_length(a, c, d)
 
-    mid_ad, mid_bc = mid_point(a, b, c, d)
+        mid_ad, mid_bc = mid_point(a, b, c, d)
 
-    is_rectangle_result = is_rectangle(mid_ad, mid_bc, diagonal_ad, diagonal_cb)
+        is_rectangle_result = is_rectangle(mid_ad, mid_bc, diagonal_ad, diagonal_cb)
 
-    if is_rectangle_result:
-        print('True')
-        print('Date točke formiraju pravokut.')
-    else:
-        print('False')
-        print('Date točke ne formiraju pravokut.')
+        if is_rectangle_result:
+            print('True')
+            print('Date točke formiraju pravokut.')
+        else:
+            print('False')
+            print('Date točke ne formiraju pravokut.')
 
-    is_point_in_rectangle(a, d, x)
-    print(diagonal_ad)
+        is_point_in_rectangle(a, d, x)
+        print(diagonal_ad)
+        print ('__________________________________________')
