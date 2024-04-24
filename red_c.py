cd1efb8f6a249477635f7f3e12557bd983674335
Dusan Rangelov <dusanrangelov@yahoo.com>
2024-04-24 18:45:56 +0200

    Third commit: Ran pylint, cleaned code

--- red_c.py
+++ red_c.py
@@ -1,80 +1,74 @@
-import numpy as np
 import math
-import sys
+import numpy as np
 
-def cordinates_from_file(filename):
-    with open('filename','r') as file:
-        lines = file.readline()
-        coordinates = [list(map(float, line.strip().split(','))) for line in lines]
-    return coordinates
 
-def fourthPointCoordiantes(A,B,C):
+def fourth_point_coordinates(a, b, c):
     """
-    Pronalazi koordinate nedostajuće točke.
+    Find coordinates of the fourth point for a given three points of a rectangle.
     """
-
-    AC = np.array(C) - np.array(A)
-    D = (tuple(np.array(B) + AC))
-    
-    print (f'Točka D je kordinata: {D}')
-    return (D)
+    ac = np.array(c) - np.array(a)
+    d = tuple(np.array(b) + ac)
+    return d
 
 
-def findDiagonalLenght(A,C):
-
-    A = np.array(A)
-    C = np.array(C)
-
-    diagonal_ad = math.sqrt((D[0]-A[0])**2 + (D[1]-A[1])**2)
-    diagonal_cb = math.sqrt((C[0]-B[0])**2 + (C[1]-B[1])**2)
-    
-    print(f'Diagonala AD: {diagonal_ad}, diagonala CB: {diagonal_cb}')
-    return diagonal_ad,diagonal_cb
-    
-def midPoint(A,B,C,D):
-    mid_point_BC = ((B[0] + C[0]) / 2, (B[1] + C[1]) / 2)
-    mid_point_AD = ((A[0] + D[0]) / 2, (A[1] + D[1]) / 2)
-
-    return mid_point_AD, mid_point_BC
+def find_diagonal_length(a, c, d):
+    """
+    Find the lengths of diagonals.
+    """
+    diagonal_ad = math.sqrt((d[0] - a[0]) ** 2 + (d[1] - a[1]) ** 2)
+    diagonal_cb = math.sqrt((c[0] - b[0]) ** 2 + (c[1] - b[1]) ** 2)
+    return diagonal_ad, diagonal_cb
 
 
-def isPointInRectangle(A,C,X=None):
-    if (X[0]>A[0] and X[0]<D[0] and X[1]<C[1] and X[1]<D[1]):
-        print ('Točka je unutar datog pravokuta.')
+def mid_point(a, b, c, d):
+    """
+    Find the midpoints of diagonals.
+    """
+    midpoint_bc = ((b[0] + c[0]) / 2, (b[1] + c[1]) / 2)
+    midpoint_ad = ((a[0] + d[0]) / 2, (a[1] + d[1]) / 2)
+    return midpoint_ad, midpoint_bc
+
+
+def is_point_in_rectangle(a, d, x):
+    """
+    Check if a point is within a rectangle defined by given points.
+    """
+    if (x[0] > a[0] and x[0] < d[0] and x[1] > a[1] and x[1] < d[1]):
+        print('Točka je unutar pravokuta.')
     else:
-        print ('Točka je van datog pravokuta.')
+        print('Točka je van pravokuta.')
 
-def is_rectangle(mid_point_AD, mid_point_BC):
-        if mid_point_AD == mid_point_BC and diagonal_ad == diagonal_cb:
-            print ('Postavljene tačke obrazuju pravokut.')
-            return True
-        else:
-            print ('Postavljene tačke ne obrazuju pravokut.')
-            return False
-             
+
+def is_rectangle(midpoint_ad, midpoint_bc, diagonal_ad, diagonal_cb):
+    """
+    Check if points form a rectangle based on midpoints and diagonal lengths.
+    """
+    if midpoint_ad == midpoint_bc and diagonal_ad == diagonal_cb:
+        return True
+    else:
+        return False
 
 
 if __name__ == '__main__':
+    a = (1, 1)
+    b = (6, 1)
+    c = (1, 6)
+    x = (2, 2)
 
+    d = fourth_point_coordinates(a, b, c)
 
-    
-    A = (1,1)
-    B = (6,1)
-    C = (1,6)
-    X = (2,2)
+    diagonal_ad, diagonal_cb = find_diagonal_length(a, c, d)
 
-    D = fourthPointCoordiantes(A,B,C)
-    
-    diagonal_ad,diagonal_cb = findDiagonalLenght(A,C)
-    
-    mid_BC, mid_AD = midPoint(A,B,C,D)
+    mid_ad, mid_bc = mid_point(a, b, c, d)
 
-    is_rectangle(mid_BC, mid_AD)
-        
-    
-    in_rectangle = isPointInRectangle(A,D,X)
-    
-    
+    is_rectangle_result = is_rectangle(mid_ad, mid_bc, diagonal_ad, diagonal_cb)
 
-    
-        
+    if is_rectangle_result:
+        print('True')
+        print('Date točke formiraju pravokut.')
+    else:
+        print('False')
+        print('Date točke ne formiraju pravokut.')
+
+    is_point_in_rectangle(a, d, x)
+    print(diagonal_ad)
